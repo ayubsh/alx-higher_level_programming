e42a60af67c895ac5a3a29875a3452e829962d0f
ayubsh <ibrahimayubsheikh@gmail.com>
2022-09-22 11:54:07 -0700

    prints name

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     import hidden_4
 
     diff = dir(hidden_4)
-    
     for a in range(0, len(diff)):
         if diff[a][:2] != "__":
             print("{}".format(diff[a]))
