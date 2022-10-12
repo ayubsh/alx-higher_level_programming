9742bf444da6905b9b2ac878d79b14f3800ce174
ayubsh <ibrahimayubsheikh@gmail.com>
2022-10-12 06:48:03 -0700

    private intance field

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+""" The square class"""
+
+
+class Square:
+    """ base class of square
+    """
+    def __init__(self, size):
+        """ Constructer method of square class
+        Args:
+            size: the size of the instance square ob
+        """
+        self.__size = size
