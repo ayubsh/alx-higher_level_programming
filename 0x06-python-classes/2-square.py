890945bbfea689584e6044bb92e30e5aa00457e0
ayubsh <ibrahimayubsheikh@gmail.com>
2022-10-12 07:01:34 -0700

    guarded prive field size in square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bi/python3
+
+""" The square Class """
+
+
+class Square:
+    """ base class of square
+    """
+    def __init__(self, size=0):
+        """ constructor method for the sqaure class
+        Args:
+            size: the size of the instance class
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
