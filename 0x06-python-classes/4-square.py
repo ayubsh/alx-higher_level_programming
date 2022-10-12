cf08aeb44da9c9b132602d32ae44bd72db84e5c8
ayubsh <ibrahimayubsheikh@gmail.com>
2022-10-12 07:51:43 -0700

    setters and getters for private field

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+""" The square Class """
+
+
+class Square:
+    """ base class body of sqaure """
+
+    def __init__(self, size=0):
+        """ constructor for the square class
+        Args:
+            size: the size sqaure instance
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ compute the area of the current instance
+        of square nad returns it
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """ size is getter that gets the private field__size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ size setter that sets the private field __size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
