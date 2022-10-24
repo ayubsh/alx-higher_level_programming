e0c7b647a191cd68c50c6dd0974ef4a5eb61955e
ayubsh <ibrahimayubsheikh@gmail.com>
2022-10-24 11:41:32 -0700

    check inherited class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,7 +13,7 @@ def inherits_from(obj, a_class):
             specified class or inherited class otherwise
             False
     """
-    if issubclass(type(obj), a_class) and type(obj) not a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
