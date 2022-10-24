caaf1e0e602a429e1568a49ce36be4b96570caf7
ayubsh <ibrahimayubsheikh@gmail.com>
2022-10-24 11:39:29 -0700

    check inherited class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" module is kind of the same """
+
+
+def inherits_from(obj, a_class):
+    """Checks weather obj is intance of class or
+        inherited class
+        Args:
+            obj: intance onject
+            a_class: class to be checked against
+        Returns:
+            True if the object is instance if the
+            specified class or inherited class otherwise
+            False
+    """
+    if issubclass(type(obj), a_class) and type(obj) not a_class:
+        return True
+    else:
+        return False
