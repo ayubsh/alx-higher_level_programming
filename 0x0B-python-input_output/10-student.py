2f03a131872e88185561ff78648efc36f0e61a93
ayubsh <ibrahimayubsheikh@gmail.com>
2022-10-25 23:47:08 +0300

    student to json to filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Student to JSON """
+
+
+class Student:
+    """ it description of student """
+    def __init__(self, first_name, last_name, age):
+        """ constructor for the student class
+        Args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of the class
+        Args:
+            attrs: the attritubes to look for
+        """
+        if isinstance(attrs, list) and\
+                all(type(e) == str for e in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
