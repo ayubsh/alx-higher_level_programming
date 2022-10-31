e709e50fc967f1b1c5b026eb3b139283a73fee4a
ayubsh <ibrahimayubsheikh@gmail.com>
2022-10-31 21:23:50 +0300

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,3 +107,23 @@ class Rectangle(Base):
                                                     self.width,
                                                     self.height)
         return s
+
+    def update(self, *args):
+        """ updates rectangle instance """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
